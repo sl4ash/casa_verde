4df7a0e1739f9ebe471f902bfa751e0756dff075
sl4ash <nicolassouza124@gmail.com>
2024-10-18 10:34:02 -0300

    .gitignore update

--- app/_components/Newsletter/components/NewsletterInput/NewsletterInput.tsx
+++ app/_components/Newsletter/components/NewsletterInput/NewsletterInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { InputWrapper, Input, IconeEmail, AssinarButton } from "./styled";
 
 function NewsletterInput() {
